829e2d21e993b5595a45fc87a806fcf403448f3c
Ryan Tan <ryantzr@Ryans-MacBook-Pro.local>
2023-02-04 02:42:24 +0800

    added new Telebot methods

--- Telegram Bot/bot.js
+++ Telegram Bot/bot.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 const e = require("express");
 const Bot = require("node-telegram-bot-api");
 let bot;
+const { handleReplyCustomer, handleStart, botState } = require("botMethods");
 const Convo = require("./MongoDBModel/ConvoModel"); //importing Convos
 
 if (process.env.NODE_ENV === "production") {
@@ -15,18 +16,36 @@ if (process.env.NODE_ENV === "production") {
 
 console.log("Bot server started in the " + process.env.NODE_ENV + " mode");
 
+//handle /start
+bot.onText(/\/start/, handleStart);
+
+//handle /auto or shld we say botMode
+bot.onText(/\/auto/, handleAuto);
+
+//handle /manual or shld we say humanMode
+bot.onText(/\/manual/, handleManual);
+
+bot.onText(/\/replyCustomer/, handleReplyCustomer);
+//still working on this part
+//I would guess we would prompt the seller with a Q and A
+//1. Chat Id with Customer (not breach of IS1108 cos its a chat id only not userid)
+//2. Message to customer
+
 bot.on("message", async (msg) => {
   console.log(msg);
   try {
     const text = msg.text;
-    console.log(text);
+    //1. Building a way to automatically forward messages to the seller whenever someone messages the bot
+    //To test the concatenation of text + msg.chat.id
+    bot.sendMessage("INPUT SELLER ID", text + " " + msg.chat.id).then(() => {});
+
     const res = await axios.get("http://13.115.58.243/predict", {
       params: { question: text },
     });
     console.log(res.data);
     const answer = res.data.answer;
 
-    //a convo instance
+    //a convo instance - do we still get the AI to run even when they are in manual mode? Yes for now
     const convo = new Convo({ question: text, answer: answer });
     console.log(convo);
     convo.save(function (err, convo) {
@@ -36,11 +55,15 @@ bot.on("message", async (msg) => {
         console.log(convo.name + "saved to Convos collection");
       }
     });
-    bot.sendMessage(msg.chat.id, answer).then(() => {});
-
+    //checking state to see if we are in manual or auto mode
+    if (botState().getIsAuto()) {
+      bot.sendMessage(msg.chat.id, answer).then(() => {});
+    }
   } catch (e) {
     console.log(e.message);
-    bot.sendMessage(msg.chat.id, "Sorry. We are unable to process your query.").then(() => {});
+    bot
+      .sendMessage(msg.chat.id, "Sorry. We are unable to process your query.")
+      .then(() => {});
   }
 });
 
---
+++ Telegram Bot/botMethods.js
@@ -0,0 +1,57 @@
+//Function to handle event when user presses "/start"
+
+import bot from "./bot";
+
+//use it as a callback for the bot.onText
+function handleStart(msg) {
+  const chatId = msg.chat.id;
+  const message = "Welcome to our shop! How can I help you today?"; //sample message
+  bot.sendMessage(chatId, message).then(() => {});
+}
+
+function botState() {
+  const [isAuto, setIsAuto] = useState(true);
+
+  const handleAuto = (msg) => {
+    const sellerChatId = msg.chat.id;
+    setIsAuto(true);
+    bot.sendMessage(
+      sellerChatId,
+      "F.R.I.D.A.Y. is handling your customer service now!"
+    );
+  };
+
+  const handleManual = (msg) => {
+    const sellerChatId = msg.chat.id;
+    setIsAuto(false);
+    bot.sendMessage(sellerChatId, "Manual mode activated!");
+  };
+
+  const getIsAuto = () => {
+    return isAuto;
+  };
+
+  return { handleAuto, handleManual, getIsAuto };
+}
+
+function handleReplyCustomer(msg) {
+  const sellerChatId = msg.chat.id;
+  // Ask the seller for the customer id and the message to the customer
+  bot.sendMessage(sellerChatId, "Please enter the customer id:").then(() => {});
+  bot.once("message", async (sellerMessage) => {
+    const customerId = sellerMessage.text;
+    bot
+      .sendMessage(
+        sellerChatId,
+        "Please enter the message to send to the customer:"
+      )
+      .then(() => {});
+    bot.once("message", async (sellerMessage) => {
+      const messageToCustomer = sellerMessage.text;
+      // Now that you have the customer id and the message to the customer, you can use them to send a message to the customer.
+      bot.sellerMessage(customerId, messageToCustomer);
+    });
+  });
+}
+
+export { handleReplyCustomer, handleStart, botState };
